2b24bde11ac0f19fc03b9f5a8446dd800c1c4be0
graham <grahamnash890729@gmail.com>
2020-08-24 09:35:34 +0200

    changed repo

--- app.js
+++ app.js
@@ -4,26 +4,38 @@ const fs = require('fs');
 
 
 //Handles the infinite scroll
-async function infiniteScroll(page,scrollDelay,groupName){
-    let index = 0;
+async function infiniteScroll(page,scrollDelay,groupName,FileStream){
+    FileStream.write(`groupid|groupName|profile_url|full_name|info1|info2\n`);
     let previousHeight;
-    let FileStream = fs.createWriteStream(`./Results/${settings.groupId}.csv`,{flags:'a'});
-    FileStream.write(`groupid|groupName|profile_url|full_name\n`);
     try{
+        let previousLength = 0;
+        let total = 0;
         while(true) {
             //Gets the element handler
-            console.log(index);
+            console.log(`${total} ${previousLength}`);
             let profiles = await page.$$('div[id*="recently_joined"]');
-            let newItems = profiles.slice(index);
+            let newItems = profiles.slice(previousLength)
             for (let profile of newItems) {
-                index+=1;
+                previousLength+=1;
+                total += 1;
                 //evaluate the <a> child of the handle -- this is the same as document.querySelector ...
                 let title = await profile.$eval('a', el => el.title)
                 let src = await profile.$eval('a', el => el.href);
+                let infoTextArray = await profile.$$eval('div[class="_60rj"]',elements=>(elements.map(el=>el.innerText)))
+               // console.log(infoTextArray);
+
                 //console.log(`${src} ${title}`)
                 //Writes data to pipe delimited file
-                FileStream.write(`${settings.groupId}|${groupName}|${src.replace(/(\?).*$/,'')}|${title}\n`);
+                FileStream.write(`${settings.groupId}|${groupName}|${src.replace(/(\?).*$/,'')}|${title}|${infoTextArray[0]}|${infoTextArray[1]}\n`);
             }
+            if(newItems.length === 0){
+                previousLength = 0;
+            }
+
+
+
+
+
             previousHeight = await page.evaluate('document.body.scrollHeight');
             await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
             await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
@@ -41,7 +53,7 @@ async function infiniteScroll(page,scrollDelay,groupName){
     const navigationPromise = page.waitForNavigation()
     await page.goto('https://www.facebook.com/')
 
-    await page.setViewport({ width: 1536, height: 733 })
+    await page.setViewport({ width: 2000, height: 900 })
 
     await navigationPromise
     const loggedin = await page.$('div[role="search"') || null;
@@ -62,7 +74,11 @@ async function infiniteScroll(page,scrollDelay,groupName){
 
 
             await page.click('button[name="login"]');
+
+            //Wait for the page to complete loading , if just check is not done the page.goto below doesn't work
             await page.waitForNavigation({ waitUntil: 'networkidle0' })
+
+            //Check if we are at the page where the sms code needs to be entered , if so the scraper waits 20 seconds for the user to do that
             const askingFor2FACode = await page.$('input[id="approvals_code"') || null;
             if(askingFor2FACode){
                 console.log("Waiting for 2FA to be entered");
@@ -83,11 +99,14 @@ async function infiniteScroll(page,scrollDelay,groupName){
         //Get the group name
         let groupName = await page.evaluate(el=> el.innerText,await page.$('a[href*="group_header"]'));
 
-        //Wait fir the members list to show
+        //Wait firs the members list to show
         await page.waitForSelector('div[id*="recently_joined"]')
 
+        let FileStream = fs.createWriteStream(`./Results/${settings.groupId}.csv`,{flags:'a'});
+
+
         //gets the member names by scrolling infinitely
-        await infiniteScroll(page,10,groupName);
+        await infiniteScroll(page,(Math.random()*10),groupName,FileStream);
 
     }catch(err){
         console.log(err);
