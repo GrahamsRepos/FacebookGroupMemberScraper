2bdb88177eb776805ef3039831ad2297dd872aa2
graham <grahamnash890729@gmail.com>
2020-08-31 15:18:52 +0200

    Added mask for make config password field

--- MakeConfig.sh
+++ MakeConfig.sh
@@ -1,10 +1,9 @@
  #!/bin/bash
 
-echo "Please enter your facebook email"
-read email
-echo "Please enter your facebook password"
-read password
-echo "Please enter the group id you want to scrape"
-read groupid
+read -p "Please enter your facebook email: " email 
+read -s -p "Please enter your password: " password
+read -p "
+Please enter the group id you want to scrape: " groupid
+
 
 echo '{"facebook_user": "'$email'","facebook_password": "'$password'","groupId": "'$groupid'"}' > './config/Config.json'
